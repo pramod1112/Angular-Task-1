b226df1b6027d2ae79311ca97ed7075a1d26a4a8
pramod11121~ <1993pramodjadhav@gmail.com>
2023-01-26 20:44:02 +0530

    change9

--- src/app/shared/services/shopping.service.ts
+++ src/app/shared/services/shopping.service.ts
@@ -4,10 +4,10 @@ import { shopping } from "../model/shoppingList.model";
 // @Injectable({providedIn : 'root'})
 export class ProductService {
   private productList = [
-    new shopping('https://thumbs.dreamstime.com/b/grocery-shopping-cart-vegetables-fruits-supermarket-background-88126559.jpg', 'Product 1', 100),
-    new shopping('https://thumbs.dreamstime.com/b/grocery-shopping-cart-vegetables-fruits-supermarket-background-88126559.jpg', 'Product 2', 110),
+    new shopping('https://thumbs.dreamstime.com/b/chicken-dum-biryani-white-bowl-traditional-indian-one-pot-dish-background-high-angle-view-156498926.jpg', 'Chicken Dum Biriyani ', 180),
+    new shopping('https://st2.depositphotos.com/4513173/6656/i/950/depositphotos_66568599-stock-photo-delicious-tandoori-chicken-legs.jpg', 'Chicken Tandoori', 250),
     // new shopping('https://5.imimg.com/data5/SELLER/Default/2021/3/KO/QG/XG/3922575/all-grocery-items-500x500.jpg','Product 2', '12345'),
-    new shopping('https://thumbs.dreamstime.com/b/grocery-shopping-cart-vegetables-fruits-supermarket-background-88126559.jpg', 'Product 3', 130)
+    new shopping('https://img.freepik.com/free-photo/crispy-fried-chicken-plate-with-salad-carrot_1150-20212.jpg', 'Fried Chicken', 240)
   ];
 
   private shoppedList: any[] = [];
